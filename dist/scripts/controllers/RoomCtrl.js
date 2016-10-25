452fd8aa160a7eb8bbf9f72fdd24541f572d6a0c
Mallory Robertson <mallory@mal.works>
2016-10-24 17:41:24 -0700

    Start checkpoint 5

--- dist/scripts/controllers/RoomCtrl.js
+++ dist/scripts/controllers/RoomCtrl.js
@@ -5,9 +5,12 @@
       this.setActiveRoom = (room) => {
         this.activeRoom = room;
         this.listMessages = Room.getMessages(this.activeRoom.$id);
-
-
+      
       };
+
+      this.createUser = function(name) {
+        this.currentUser = name
+      }
     }
     
     angular
